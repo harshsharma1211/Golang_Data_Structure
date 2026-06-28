aa91d78b43cfb02d88daf306ace86e67ef3cc8e4
HS <harsh.sharma1@kdksoftware.com>
2026-06-29 02:02:20 +0530

    added the insert at beginning

--- Array/InsertAtBegining/main.go
+++ Array/InsertAtBegining/main.go
@@ -1,4 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var arr = [5]int{24, 34, 11, 56, 78}
+	size := 4
+	Val := 3751
+
+	//right shift the elements of the array
+	for i := size; i > 0; i-- {
+
+		arr[i] = arr[i-1]
+
+	}
+
+	arr[0] = Val
+	fmt.Println("Array after inserting an element at the beginning: ", arr)
+
 }
